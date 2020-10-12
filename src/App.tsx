26bdae45bc3c9e8c397c4220d8fdeb96a1f10037
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-10-12 18:36:28 -0300

    Criando pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+import './styles/global.css';
+import './styles/pages/lading.css';
+import logoImg from './assets/images/logo.svg';
 
-function App() {
-  return (
-    <div>
-      <h1>Ola mundo</h1>
+const App: React.FC = () => (
+  <div id="page-landing">
+    <div className="content-wrapper">
+      <img src={logoImg} alt="happy" />
+      <main>
+        <h1>Leve felicidade para o mundo</h1>
+        <p>Visite orfanatos e mude o dia de muitas crianças</p>
+      </main>
+      <div className="location">
+        <strong>Iguatama</strong>
+        <span>Minas Gerais</span>
+      </div>
+      <a href="/" className="enter-app">
+        <FiArrowRight size={26} color="rgba(0,0,0,.6)" />
+      </a>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
